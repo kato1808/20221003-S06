f09fcf27fdbd9e94191a4acfa21f4faaaf1e2b6d
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-10-03 10:47:47 +0900

    20221003

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import ReactDom from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// ReactDom.render(<App /> ,document.getElementById("root"));
 
-root.render(
+ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
